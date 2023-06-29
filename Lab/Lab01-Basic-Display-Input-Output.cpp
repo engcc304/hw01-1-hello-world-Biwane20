6b30331a8060418478679c3a003a46acc63803be
Biwane20 <biwbew20@gmail.com>
2023-06-29 14:44:59 +0700

    1

--- Lab/Lab01-Basic-Display-Input-Output.cpp
+++ Lab/Lab01-Basic-Display-Input-Output.cpp
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
-int main()
-{
-    char Name[50];
-    int Age = 0;
-    printf("Enter your name: ");
-    scanf("%s",Name);
+int main() {
+    char Name[50] ;
+    int  Age = 0 ;
+    printf("Enter your name: "); 
+    scanf("%s", Name);
     printf("Enter your age: ");
     scanf("%d",&Age);
-    printf("- - - - - -\n");
+    printf("- - - - - -\n" );
     printf("Hello %s\n",Name); //TODO: #14 Say hello to user.
     printf("Age = %d\n",Age); //TODO: #15 display user their age number plus 1.
-    return 0;
-} // end main functionw
+    return 0 ;
+}//end main function
